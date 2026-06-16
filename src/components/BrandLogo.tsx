695270c69a2cdf75f3c761dcecf46576c33b0f7c
Muhammad Taibe <taibejaved485@gmail.com>
2026-06-16 17:32:12 +0500

    nav bar logo

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -8,7 +8,7 @@ export default function BrandLogo({ isDark = false }: BrandLogoProps) {
   return (
     <div className="flex items-center select-none group cursor-pointer">
       <img 
-        src="/src/assets/images/godriveify_full_logo_v1_1781611923000_1781611922364.jpg" 
+        src="/static/godriveify-logo.jpg" 
         alt="GoDriveify Logo" 
         className="h-20 w-auto object-contain relative z-10 transform transition-transform duration-500 group-hover:scale-105"
         referrerPolicy="no-referrer"
